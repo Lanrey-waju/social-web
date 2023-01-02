4afb5b6b3514f88d0d2f2b28d5edc4441d2bccf0
Abdulmumin <lanreywaju97@gmail.com>
2023-01-02 23:47:44 +0100

    Adding bassic authentication functionalities

---
+++ bookmarks/account/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path("login", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "password-change/",
+        auth_views.PasswordChangeView.as_view(),
+        name="password_change",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_creset_done",
+    ),
+]
