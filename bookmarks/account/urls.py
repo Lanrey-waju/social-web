ff25d986c8bbe8aedc0c064ba6827d1710f34fb1
Abdulmumin <lanreywaju97@gmail.com>
2023-01-03 23:07:13 +0100

    Adjusted login template

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,9 +5,7 @@ urlpatterns = [
     path("login", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path(
-        "password-reset/",
-        auth_views.PasswordResetView.as_view(),
-        name="password_reset",
+        "password-reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
     ),
     path(
         "password-reset/done",
