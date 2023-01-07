a213f8a494dda47e9c8770b9a00404b19270c48a
Abdulmumin <lanreywaju97@gmail.com>
2023-01-07 23:41:41 +0100

    Created a CBV signup view and its html template

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 class HomePageView(generic.TemplateView):
     template_name = "home.html"
+
+
+class SignUpPageView(generic.CreateView):
+    template_name = "signup.html"
+    success_url = reverse_lazy("login")
+    form_class = CustomUserCreationForm
